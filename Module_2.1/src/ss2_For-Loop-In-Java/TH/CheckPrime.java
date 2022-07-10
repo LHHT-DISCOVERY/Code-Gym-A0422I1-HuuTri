d0ae4d044111c03d95a9af7fbb46531b46bd894a
your name <lytri102@gmail.com>
2022-07-10 13:41:18 +0700

    BT ss2

---
+++ Module_2.1/src/ss2_For-Loop-In-Java/TH/CheckPrime.java
@@ -0,0 +1,29 @@
+package ss2_ForLoopInJava.TH;
+
+import java.util.Scanner;
+
+public class CheckPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Nhập vào số cần kiểm tra : ");
+        number = scanner.nextInt();
+        if (number <= 0 || number == 1) {
+            System.out.println(number + " is Not Prime");
+        } else if (number == 2) {
+            System.out.println(number + " is Prime");
+        } else {
+            boolean isFlag = true;
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    System.out.println(number + " is Not Prime");
+                    isFlag = false;
+                    break;
+                }
+            }
+            if (isFlag == true) {
+                System.out.println(number + " is Prime");
+            }
+        }
+    }
+}
