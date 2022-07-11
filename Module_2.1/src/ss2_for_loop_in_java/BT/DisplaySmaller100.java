8f73729df7249c8bbe80df3d55d66b1f335bda84
your name <lytri102@gmail.com>
2022-07-11 23:17:54 +0700

    "BT ss2 for loop"

---
+++ Module_2.1/src/ss2_for_loop_in_java/BT/DisplaySmaller100.java
@@ -0,0 +1,29 @@
+package ss2_for_loop_in_java.BT;
+
+public class DisplaySmaller100 {
+    public static void main(String[] args) {
+        int number = 2 ;
+        while (true) {
+            boolean isFlag = true;
+            if (number == 2) {
+                System.out.println(number);
+            } else {
+                for (int i = 2; i < number; i++) {
+                    if (number % i == 0) {
+                        isFlag = false;
+                        break;
+                    }
+                }
+                if (isFlag == true) {
+                    System.out.println(number);
+                } else {
+                    isFlag = true;
+                }
+            }
+            number ++ ;
+            if(number > 100){
+                break;
+            }
+        }
+    }
+}
