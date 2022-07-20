aaf1670a1afa9f430430d8e4a1257fd37e4d6f5b
your name <lytri102@gmail.com>
2022-07-20 18:45:06 +0700

    "BT ss6"

--- Module_2.1/src/ss5_access_modifier_static_method_static_property/static_property/Car.java
+++ Module_2.1/src/ss5_access_modifier_static_method_static_property/static_property/Car.java
@@ -3,7 +3,7 @@ package ss5_access_modifier_static_method_static_property.static_property;
 public class Car {
     private String name;
     private String engine;
-    public static int numberOfCar = -1;
+    public static int numberOfCar ;
 
     public Car(String name, String engine) {
         this.setName(name);
