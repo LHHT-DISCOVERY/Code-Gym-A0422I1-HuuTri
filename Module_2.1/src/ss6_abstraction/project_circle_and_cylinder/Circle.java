9af8a1245a957f3c5da0344c8d11d1b4e68c72d4
your name <lytri102@gmail.com>
2022-07-20 18:44:13 +0700

    "BT ss6"

---
+++ Module_2.1/src/ss6_abstraction/project_circle_and_cylinder/Circle.java
@@ -0,0 +1,36 @@
+package ss6_abstraction.project_circle_and_cylinder;
+
+public class Circle {
+    private Double radius;
+    private String color;
+
+    public Circle() {
+        this.setRadius(0.0);
+        this.setColor("No Color");
+    }
+
+    public Circle(double radius, String color) {
+        this.setRadius(radius);
+        this.setColor(color);
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toString() {
+        return " Color is : " + this.getColor() + "\n Radius is : " + this.getRadius();
+    }
+}
