0f3514401a256d240089c2d2ab01adde6ec4d54f
your name <lytri102@gmail.com>
2022-07-25 18:51:25 +0700

    ok

---
+++ Module_2.1/src/ss7_abstrac_interface/exercise/interface_resizeable/Resizeable.java
@@ -0,0 +1,5 @@
+package ss7_abstrac_interface.interface_resizeable;
+
+public interface Resizeable {
+    double resize(double percent);
+}
---
+++ Module_2.1/src/ss7_abstrac_interface/exercise/interface_resizeable/SquareTest.java
@@ -0,0 +1,12 @@
+package ss7_abstrac_interface.interface_resizeable;
+
+public class SquareTest {
+    public static void main(String[] args) {
+        Square square = new Square();
+        System.out.println(square);
+        Square square1 = new Square(3);
+        System.out.println(square1);
+        Square square2 = new Square("red", false, 3);
+
+    }
+}
