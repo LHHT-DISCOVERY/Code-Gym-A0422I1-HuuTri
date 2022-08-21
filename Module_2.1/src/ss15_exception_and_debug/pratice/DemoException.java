de0e5b1cd888b564e5af1b94be8a74fb133a6e0c
DISCOVERY <lytri102@gmail.com>
2022-08-21 22:39:48 +0700

    "ok "

--- Module_2.1/src/ss15_exception_and_debug/pratice/DemoException.java
+++ Module_2.1/src/ss15_exception_and_debug/pratice/DemoException.java
@@ -7,43 +7,43 @@ import java.util.Scanner;
 
 public class DemoException {
     public static void main(String[] args) {
-        System.out.println("Nhap so a: ");
+//        System.out.println("Nhap so a: ");
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        System.out.println("Nhap so b: ");
-        int b = scanner.nextInt();
-        System.out.println("a/b = " + a/b);
-        System.out.println("Success");
-        testException();
+//        int a = scanner.nextInt();
+//        System.out.println("Nhap so b: ");
+//        int b = scanner.nextInt();
+//        System.out.println("a/b = " + a/b);
+//        System.out.println("Success");
+//        testException();
+//
+//        try {
+//            testException2();
+//        } catch (ParseException e) {
+//            System.out.println(e.getMessage());
+//        }
 
-        try {
-            testException2();
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            testExceptionDefine();
-        } catch (DateException e) {
-            System.out.println(e.getMessage());
-        }
+//        try {
+//            testExceptionDefine();
+//        } catch (DateException e) {
+//            System.out.println(e.getMessage());
+//        }
 
 
         // check validate
 //        Scanner scanner = new Scanner(System.in);
-        System.out.println("nhap vao n: ");
-        int n = scanner.nextInt();
-        while (true) {
-            try {
-                if (checkNumber(n)) {
-                    break;
-                }
-            } catch (NumberException e) {
-                System.out.println(e.getMessage());
-                System.out.println("nhap lai n: ");
-                n = scanner.nextInt();
-            }
-        }
+//        System.out.println("nhap vao n: ");
+//        int n = scanner.nextInt();
+//        while (true) {
+//            try {
+//                if (checkNumber(n)) {
+//                    break;
+//                }
+//            } catch (NumberException e) {
+//                System.out.println(e.getMessage());
+//                System.out.println("nhap lai n: ");
+//                n = scanner.nextInt();
+//            }
+//        }
 
         // try finally
         try {
@@ -53,55 +53,55 @@ public class DemoException {
         }
     }
 
-    public static void testException() {
-        Date date;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap vao ngay (dd/MM/yyyy):");
-        String dateAsString = scanner.nextLine();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            date = format.parse(dateAsString);
-            System.out.println("None error");
-            System.out.println(date.toString());
-//            int a = 4 / 0;
-            "aaa".charAt(10);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Exception: " + e.getMessage());
-        } finally {
-            System.out.println("finally");
-        }
-        System.out.println("success");
-    }
+//    public static void testException() {
+//        Date date;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhap vao ngay (dd/MM/yyyy):");
+//        String dateAsString = scanner.nextLine();
+//        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+//        try {
+//            date = format.parse(dateAsString);
+//            System.out.println("None error");
+//            System.out.println(date.toString());
+////            int a = 4 / 0;
+//            "aaa".charAt(10);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        } catch (ArithmeticException e) {
+//            System.out.println(e.getMessage());
+//        } catch (Exception e) {
+//            System.out.println("Exception: " + e.getMessage());
+//        } finally {
+//            System.out.println("finally");
+//        }
+//        System.out.println("success");
+//    }
 
-    public static void testException2() throws ParseException {
-        Date date;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap vao ngay (dd/MM/yyyy):");
-        String dateAsString = scanner.nextLine();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        date = format.parse(dateAsString);
-        System.out.println("None error");
-        System.out.println(date.toString());
-    }
+//    public static void testException2() throws ParseException {
+//        Date date;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhap vao ngay (dd/MM/yyyy):");
+//        String dateAsString = scanner.nextLine();
+//        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+//        date = format.parse(dateAsString);
+//        System.out.println("None error");
+//        System.out.println(date.toString());
+//    }
 
-    public static void testExceptionDefine() throws DateException {
-        Date date;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Nhap vao ngay (dd/MM/yyyy):");
-        String dateAsString = scanner.nextLine();
-        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-        try {
-            date = format.parse(dateAsString);
-        } catch (ParseException e) {
-            throw new DateException("Ngay khong hop le");
-        }
-        System.out.println("None error");
-        System.out.println(date.toString());
-    }
+//    public static void testExceptionDefine() throws DateException {
+//        Date date;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Nhap vao ngay (dd/MM/yyyy):");
+//        String dateAsString = scanner.nextLine();
+//        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+//        try {
+//            date = format.parse(dateAsString);
+//        } catch (ParseException e) {
+//            throw new DateException("Ngay khong hop le");
+//        }
+//        System.out.println("None error");
+//        System.out.println(date.toString());
+//    }
 
     public static boolean checkNumber(int n) throws NumberException {
         if (n < 0) {
