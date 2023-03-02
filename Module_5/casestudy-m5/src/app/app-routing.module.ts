6ccb0bed0986d9c5ebf0fc706052523c13a63ecf
LHHT-DISCOVERY <lytri102@gmail.com>
2023-03-02 23:58:04 +0700

    OK

--- Module_5/casestudy-m5/src/app/app-routing.module.ts
+++ Module_5/casestudy-m5/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
+import { EditServiceComponent } from './service/edit-service/edit-service.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { CustomerAddComponent } from './customer/customer-add/customer-add.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ServiceListComponent } from './service/service-list/service-list.component';
+import { ServiceAddComponent } from './service/service-add/service-add.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add-service', component: ServiceAddComponent},
+  {path: '', component: ServiceListComponent},
+  {path: 'edit-service', component: EditServiceComponent},
+  {path: 'list-customer', component: CustomerListComponent},
+  {path: 'add-customer', component: CustomerAddComponent},
+  {path: 'edit-customer', component: CustomerEditComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Module_5/casestudy-m5/src/app/app.module.ts
+++ Module_5/casestudy-m5/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ServiceListComponent } from './service/service-list/service-list.component';
+import { ServiceAddComponent } from './service/service-add/service-add.component';
+import { EditServiceComponent } from './service/edit-service/edit-service.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerAddComponent } from './customer/customer-add/customer-add.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,12 @@ import { ServiceListComponent } from './service/service-list/service-list.compon
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
-    ServiceListComponent
+    ServiceListComponent,
+    ServiceAddComponent,
+    EditServiceComponent,
+    CustomerListComponent,
+    CustomerAddComponent,
+    CustomerEditComponent
   ],
   imports: [
     BrowserModule,
