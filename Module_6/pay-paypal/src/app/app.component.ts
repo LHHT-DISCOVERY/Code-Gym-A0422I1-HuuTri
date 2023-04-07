c5f84df0988e2cc1bd4a01bf2da8aaf22639590c
LHHT-DISCOVERY <lytri102@gmail.com>
2023-04-08 00:28:56 +0700

    update task M6

---
+++ Module_6/pay-paypal/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {render} from "creditcardpayments/creditCardPayments";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'pay-paypal';
+
+  constructor() {
+    render({
+      id:"#myPaypalButtons",
+      currency:"USD",
+      value:"100.00",
+      onApprove:(details => {
+        alert("Transaction Successful")
+      })
+    })
+
+  }
+
+}
---
+++ Module_6/pay-paypal/src/app/paypal-buttons/paypal-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paypal-buttons',
+  templateUrl: './paypal-buttons.component.html',
+  styleUrls: ['./paypal-buttons.component.css']
+})
+export class PaypalButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
